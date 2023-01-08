eea5c124a6b0b23d271a493892e0d532a66c3506
Chandrasekhar-koneti <konetichandra123@gmail.com>
2023-01-08 11:02:56 +0530

    react task7

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import ExpenseItem from './components/ExpenseItem.js'
+function App() {
+  return (
+    <div>
+     {/* <h2> Lets start</h2> */}
+     <ExpenseItem></ExpenseItem>
+     
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return <div>
+        <div> <h2>ExpenseItem</h2></div>
+        <div>
+            <h4>Food RS.150</h4>
+            <h4>Petrol RS.100</h4>
+        </div>
+    </div>
+}
+
+export default ExpenseItem
