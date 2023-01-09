d424f80b0df341251e61733b96ccdfff70303b16
Chandrasekhar-koneti <117808164+Chandrasekhar-koneti@users.noreply.github.com>
2023-01-09 20:45:47 +0530

    task 10 react

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,20 +1,15 @@
-
+import ExpenseDate from './ExpenseDate'
 import './expense.css'
-function ExpenseItem(){
-    const expensedes='Car Insurance'
-    const expensecost='RS.2150'
-    const location='Hyderbad'
-
-
-
-    return (<div><h1>EXPENSE LIST</h1> 
+import './ExpenseDate.css'
+function ExpenseItem(props){
+    return ( 
     <div className='expense-item'>
-    <div><h2>{location}</h2></div>
+        <ExpenseDate date={props.date}></ExpenseDate>
 
-        <div className='expense-item__description'><h2>{expensedes}</h2> </div>
+        <div className='expense-item__description'><h2>{props.title}</h2> </div>
 
-        <div className='expense-item__price'>{expensecost}</div>
-    </div>
+        <div className='expense-item__price'>{props.amount}</div>
+    
     </div>)
 }
 
